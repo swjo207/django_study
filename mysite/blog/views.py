6a60cb1825ef0c2461abb16f838eb1bcdb1dd893
swjo207 <swjo207@gmail.com>
2016-03-22 14:27:12 +0900

    retrieve post by similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -58,12 +59,17 @@ def post_detail(request, year, month, day, post):
 			new_comment.save()
 	else:
 		comment_form = CommentForm()
+	# 유사 글을 보여준다.
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
 	return render(request,
               'blog/post/detail.html',
               {'post': post,
 				'comments': comments,
-				'comment_form': comment_form})
+				'comment_form': comment_form,
+				'similar_posts': similar_posts})
 
 def post_share(request, post_id):
 	# retrieve post by id
